d2e8fa1c81ae3d8cd5de4e0a0efebb827a67a835
Jonathan Bahena <jonathan.mb93@gmail.com>
2021-09-13 18:14:44 -0500

    added carted product actions

--- app/controllers/carted_product_controller.rb
+++ app/controllers/carted_product_controller.rb
@@ -1,2 +1,21 @@
 class CartedProductController < ApplicationController
+  before_action :authenticate_user
+  def index
+    carted_products = CartedProduct.all
+    render json: carted_products
+  end
+
+  def create
+    carted_product = CartedProduct.new(
+      product_id: params[:product_id],
+      user_id: current_user.id,
+    )
+    if carted_product.save
+      render json: post
+    else
+      render json: {errors: post.errors.full_messages},
+      status: :unprocessable_entity
+    end
+  end
+
 end
